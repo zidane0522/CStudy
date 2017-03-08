6e6f3072135017008278ed4e395c8e7199c5a418
zidane0522 <lishen1990@163.com>
2017-03-08 16:20:43 +0800

    2

--- CProject/Main.c
+++ CProject/Main.c
@@ -1,72 +1,143 @@
 #include <stdio.h>
+
+
+int count_bits()
+{
+	unsigned num = ~0U;
+	int count = 0;
+	while (num)
+	{
+		
+		if (num & 1U)
+		{
+			num >>= 1;
+			count++;
+		}
+	}
+	return count;
+}
+
+void print_bits(unsigned num)
+{
+	int bitcount = count_bits();
+	for (int i = bitcount-1; i >= 0; i--)
+	{
+		putchar((num >> i) & 1U ? '1' : '0');
+	}
+}
+
+
+
+unsigned rrotate(unsigned x,int n)
+{
+	return x >> n;
+}
+
+unsigned lrotate(unsigned x, int n)
+{
+	return x << n;
+}
+
+//或
+unsigned set(unsigned x, int pos)
+{
+	if (pos<1)
+	{
+		puts("wrong");
+		scanf_s("%d",pos);
+	}
+	unsigned p = 1U;
+	return x|(pos==1?p: p <<(pos-1));
+}
+
+//与
+unsigned reset(unsigned x, int pos)
+{
+	unsigned p = 1U;
+	return x&(~p << (pos - 1));
+}
+
+//异或
+unsigned inverse(unsigned x, int pos) 
+{
+	if (pos<1)
+	{
+		puts("wrong");
+		scanf_s("%d", pos);
+	}
+	unsigned p = 1U;
+	return x ^ (pos == 1 ? p : p << (pos - 1));
+};
+
+//或
+unsigned set_n(unsigned x,int pos,int n) 
+{
+	unsigned p = 1U;
+	for (int i = 1; i < n; i++)
+	{
+		p <<= 1;
+		p= p | 1U;		
+	}
+	return x|(p <<(pos - 1));
+};
+
+//与
+unsigned reset_n(unsigned x, int pos, int n)
+{
+	unsigned p = 1U;
+	for (int i = 1; i < n; i++)
+	{
+		p <<= 1;
+		p = p & 1U;
+	}
+	return  x&(~p << (pos - 1));
+};
+
+unsigned inverse_n(unsigned x, int pos, int n)
+{
+	unsigned p = 1U;
+	for (int i = 1; i < n; i++)
+	{
+		p <<= 1;
+		p = p | 1U;
+	}
+	return x ^ (p << (pos - 1));
+}
+
 int main(void)
-{ 
-	puts("找出1000以内的所有质数");
+{
+	unsigned num;
+	puts("请输入一个非负整数");
+	scanf_s("%u",&num);
+	printf("二进制表示为\n");
+	print_bits(num);
 
-	//找出1000开平方的整数范围
+	int n;
+	printf("\n输入向右移动的位数");
+	scanf_s("%d",&n);
 
-	//生成质数数组
+	printf("移动后变为%u\n",rrotate(num,n));
+	print_bits(rrotate(num, n));
 
-	//总结计算次数
+	printf("\n输入向左移动的位数");
+	scanf_s("%d", &n);
+	printf("移动后变为%u", lrotate(num, n));
+	print_bits(lrotate(num, n));
 
-	//二分法确定1000开平方的整数范围
-	int left=2, right=1000;
-	int limit;
+	printf("\n输入变位的位数");
+	scanf_s("%d", &n);
+	printf("\n变位为1后为%u", set(num, n));
+	printf("\n变位为0后为%u", reset(num, n));
+	printf("\n变位为反后为%u", inverse(num, n));
 
-	while (right-left>1)
-	{
-		int temp = (left + right) / 2;
-		if (temp*temp>1000)
-		{
-			right = temp;
-		}
-		else
-		{
-			left = temp;
-		}
-	}
-	limit = right;
-	int zhishu[500] = { 2 };
-	int zhishuCount = 1;
-	int zhishuFlag;
-	int limitflag = 0;
-	int jlimit = zhishuCount;
-	int counter = 0;
-	//生成质数数组，长度500,最大值limit
-	for (int i = 3; i <= 1000; i+=2)
-	{
-		zhishuFlag = 1;
-		for (int j = 0;j < jlimit; j++)
-		{
-			counter++;
-			if (i%zhishu[j]==0)
-			{
-				zhishuFlag = 0;
-				break;
-			}
-		}
-		if (zhishuFlag)
-		{
-			counter++;
-			if (limitflag==0)
-			{
-				if (i>limit)
-				{
-					limitflag = 1;
-					
-				}
-				else
-				{
-					jlimit++;
-				}
-			}
-			zhishu[zhishuCount++] = i;
-		}
-	}
-	printf("运算次数%d\n",counter);
-	puts("质数列表");
-	for (int i = 0; i < zhishuCount; i++)
-	{
-		printf("%d\n", zhishu[i]);
-	}
+	int pos;
+	printf("\n输入开始位");
+	scanf_s("%d",&pos);
+	printf("\n输入变位长度");
+	scanf_s("%d", &n);
+
+	printf("\n变位为1后为%u", set_n(num, pos, n));
+	printf("\n变位为0后为%u", reset_n(num, pos, n));
+	printf("\n变位取反后为%u", inverse_n(num, pos, n));
+	return(0);
 }
